465a7b4f95e883922e981a58e6497f1b53c1464f
Vinitha M <112820995+Vinitha-27@users.noreply.github.com>
2022-09-07 20:13:01 +0530

    Create Mul.java

---
+++ Mul.java
@@ -0,0 +1,9 @@
+Java program to multiply two floating point numbers.
+ import java.io.*;
+ Public class Multiply{
+   float a=2.0f;
+   float b=3.5f;
+   float mul=a*b;
+      System.out.println("The multiplication is:"+ mul);
+ }
+}
