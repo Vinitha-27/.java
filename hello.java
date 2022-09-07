dd91e6635d8ec0730997cfdc9e8478b1be8e90ca
Vinitha M <112820995+Vinitha-27@users.noreply.github.com>
2022-09-07 08:32:09 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,8 @@
+import java.util.scanner;
+Public class hello world
+{
+  Public static void main (String args[]){
+  Scanner reader=new scanner (System.in);
+  System.out.println ("Hello World");
+ }
+}
