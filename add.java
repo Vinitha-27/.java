5f076f45dfe574226e5f8923e2dbc41f48c766bc
Vinitha M <112820995+Vinitha-27@users.noreply.github.com>
2022-09-07 11:59:47 +0530

    Create add.java

---
+++ add.java
@@ -0,0 +1,9 @@
+Java program to add two integers.
+  import java.io.util*;
+   class main{
+      Public static void main (String args[]){
+      int a=5,b=10;
+      int add=a+b;
+      System.out.println("the addition is:"+add);
+  }
+}
