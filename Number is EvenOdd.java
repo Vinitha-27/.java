29af5d0f6fa8dfc4fa87c80cecf459ee94f87e0b
Vinitha M <112820995+Vinitha-27@users.noreply.github.com>
2022-09-09 08:19:00 +0530

    Create Number is EvenOdd.java

---
+++ Number is EvenOdd.java
@@ -0,0 +1,13 @@
+2)1. Java program to check whether a number is Even or Odd.
+    
+import java.utill.Scanner;
+Public class EvenOdd{
+    {
+        Scanner reader=new Scanner(System.in);
+        int number=reader.nextInt();
+        if(num %2==0)
+            System.out.println("number is Even");
+        else
+            System.out.println("number is Odd");
+    }
+}
