4f049e5b3bf9d393b08bfbdf4c9ed5ad73169849
Vinitha M <112820995+Vinitha-27@users.noreply.github.com>
2022-09-12 20:00:35 +0530

    Create Factorial .java

---
+++ Factorial .java
@@ -0,0 +1,15 @@
+4)3. Java program to find factorial of a number
+
+import java.io.*;
+public class Factorial{
+  public static void main(String args[])
+  {
+    int i,fact=1;
+    int num=10;
+    for(i=1;i<=num;i++)
+    {
+         fact=fact*i;
+          System.out.println("The Factorial of +"number is:"+fact);
+     }
+   }
+   }
