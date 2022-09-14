da895aa373f7048fb85bc11418568dde48311834
Vinitha M <112820995+Vinitha-27@users.noreply.github.com>
2022-09-14 08:27:57 +0530

    Create Alphabets(A to Z) .java

---
+++ Alphabets(A to Z) .java
@@ -0,0 +1,13 @@
+6)2. Java program to Display Alphabets (A to Z) using loop
+
+import java.io.*;
+Public class Alphabets{
+  public static void main(String args[])
+  {
+    char ch;
+    System.out.print("Displaying Alphabets from A to Z /n");
+    for(ch='A', ch<='Z'," ++ch)
+        System.out.println(ch+ "");
+        }
+        }
+    
