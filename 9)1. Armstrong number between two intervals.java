e87c64d7c3568edc4a6495f2d8d9873339af9aff
Vinitha M <112820995+Vinitha-27@users.noreply.github.com>
2022-09-17 11:11:06 +0530

    Create 9)1. Armstrong number between two intervals.java

---
+++ 9)1. Armstrong number between two intervals.java
@@ -0,0 +1,30 @@
+Java program to display armstrong number between two intervals
+
+import java.io.*;
+public class Main {
+  public static void main(String args[])
+  {
+    int low = 222, high = 2222;
+    for(int number = low + 1; number < high; ++number)
+    int digits = 0;
+    int result = 0; 
+    int original number = number;
+    while(original number! = number) 
+    { 
+      originalNumber /= 10;
+      ++digits;
+    }
+    originalNumber = number;
+    while(originalNumber != 0) {
+      int remainder = originalnumber % 10)
+      result +=Math.originalNumber(Remainder,digits);
+      remainder /=10;
+     if(result==Number)
+     {
+       System.out.println("the number is");
+     }
+    }
+  }
+        
+    
+    
