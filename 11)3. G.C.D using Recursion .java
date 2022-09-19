4e4f6da4f818982afee434dd8727cb113b75e2df
Vinitha M <112820995+Vinitha-27@users.noreply.github.com>
2022-09-19 12:31:32 +0530

    Create 11)3. G.C.D using Recursion .java

---
+++ 11)3. G.C.D using Recursion .java
@@ -0,0 +1,19 @@
+Java program to Find G.C.D using Recursion.
+ 
+import java.io.*; 
+public class GCD {
+  puiblic static void main(String args[])
+  {
+    int n1 = 200 ,n2 =30;
+    int hcf = hcf(n1, n2);
+    System.out.print("G.C.D of %d and %d is %d");
+  }
+  public static int hcf(int2, n1 %n2);
+  {
+    if (n2 ! =0)
+      return hcf(n2, n1 % n2)
+    else
+      return n1;
+  }
+}
+
