b70736b18a1ca8486cc1ba90fa1c8f3bddd577d9
Vinitha M <112820995+Vinitha-27@users.noreply.github.com>
2022-09-26 14:01:14 +0530

    Create 18)2. Print an Array .java

---
+++ 18)2. Print an Array .java
@@ -0,0 +1,13 @@
+Java program to Print an Array.
+import java.io*;
+public class Array {
+  public static void main(String args[])
+  {
+    int[] array ={1,2,3,4,5};
+    for (int element: array) 
+    {
+      System.out.println("element");
+    }
+  }
+}
+
