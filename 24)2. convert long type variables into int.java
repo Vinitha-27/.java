7ef581c74fc88533efd45a263fa049d2461f9d85
Vinitha M <112820995+Vinitha-27@users.noreply.github.com>
2022-10-03 23:48:15 -0700

    Create 24)2. convert long type variables into int.java

---
+++ 24)2. convert long type variables into int.java
@@ -0,0 +1,16 @@
+Java program to convert long type variables into int
+
+import java.io.*;
+public class Main {
+  public static void main(String args[])
+  {
+    long a = 3213331L;
+    long b = 45624151L;
+    
+    int c = (int)a;
+    int d = (int)b;
+    
+   System.out.println(c);
+   System.out.println(d);
+  }
+}
