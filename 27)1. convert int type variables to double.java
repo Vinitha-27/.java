b7a8670a0a3b81a6606f6540b74acf17373349c1
Vinitha M <112820995+Vinitha-27@users.noreply.github.com>
2022-10-07 04:59:37 -0700

    Create 27)1. convert int type variables to double.java

---
+++ 27)1. convert int type variables to double.java
@@ -0,0 +1,16 @@
+Java program to convert int type variables to double.
+  
+import java.io.*;
+public class Main {
+  public static void main(String args[])
+  {
+    int a = 22;
+    int b = 10;
+    
+    double c = a;
+    double d = b;
+    
+    System.out.println(c);
+    System.out.println(d);
+  }
+}
