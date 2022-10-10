2a15cdd2bbb0381e0d2a60181723d0744bfccd94
Vinitha M <112820995+Vinitha-27@users.noreply.github.com>
2022-10-10 04:41:43 -0700

    Create 30)2. Create File and Write to the file.java

---
+++ 30)2. Create File and Write to the file.java
@@ -0,0 +1,25 @@
+Java program to Create File and Write to the File
+
+import java.io.File;
+
+class Main {
+  public static void main(String args[])
+  {
+    File file = new File("JavaFile.java");
+    
+    try {
+      boolean value = file.createNewFile();
+      if (value)
+      {
+        System.out.println("New Java File is created");
+      }
+      else
+      {
+        System.out.println("The file already exists ");
+      }
+      catch(Exception e)
+      {
+        e.getStackTrace();
+      }
+    }
+  }
