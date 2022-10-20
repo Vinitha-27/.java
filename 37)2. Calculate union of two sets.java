e0e2bad9fea72ac19246d3cb323b4d2b1e6ef0d7
Vinitha M <112820995+Vinitha-27@users.noreply.github.com>
2022-10-20 07:48:27 -0700

    Create 37)2. Calculate union of two sets.java

---
+++ 37)2. Calculate union of two sets.java
@@ -0,0 +1,22 @@
+Java program to Calculate union of two sets.
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+ public static void main(String args[])
+ {
+    Set<Integer> evenNumbers = new  HashSet<>();
+    evenNumbers.add(2);
+    evenNumbers.add(4);
+    System.out.println("Set1: " + evenNumbers);
+    
+    Set<Integer> numbers = new HashSet<>();
+    numbers.add(1);
+    numbers.add(3);
+    System.out.println("Set2: " + numbers);
+    
+    numbers.addAll(evenNumbers);
+    System.out.println("Union is : " + numbers);
+  }
+ }
