36a2cbf70a0282ee5ef2258241219b85dbc4ad31
Vinitha M <112820995+Vinitha-27@users.noreply.github.com>
2022-12-22 07:49:53 -0700

    Create 44)java program for pattern.java

---
+++ 44)java program for pattern.java
@@ -0,0 +1,19 @@
+java program for right triangle star pattern
+
+import java.io.*
+public class RightTrianglePattern
+{
+  public static void main(String args[])
+  {
+    int i,j,row=5;
+    for(i=0;j<=i;i++)
+    {
+     for(j=0;j<=i;j++)
+     {
+       System.out.print("*");
+     }
+      System.out.println();
+    }
+  }
+}
+       
