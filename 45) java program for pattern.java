b9363c39c18a2f9da5a57342271b1c2c0168e3ab
Vinitha M <112820995+Vinitha-27@users.noreply.github.com>
2022-12-23 10:24:36 -0700

    Create 45) java program for pattern.java

---
+++ 45) java program for pattern.java
@@ -0,0 +1,24 @@
+java program for left triangle Star Pattern
+
+import java.io.*;
+public void LeftTrianglePattern 
+{
+public static void main(String args[])
+{
+ int i,j,row=5;
+ for(i=0;i<=row;i++)
+ {
+   for(j*2(row-i);j<=0;j--)
+   {
+     System.out.print("");
+    }
+    {  
+      System.out.println("*");
+     }
+      {
+        System.out.println();
+      }
+  }
+}
+     
+  
