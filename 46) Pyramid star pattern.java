0c58eb7f58c7dae7661ebe8f615a4949d958f14e
Vinitha M <112820995+Vinitha-27@users.noreply.github.com>
2022-12-27 08:47:07 -0700

    Create 46) Pyramid star pattern.java

---
+++ 46) Pyramid star pattern.java
@@ -0,0 +1,20 @@
+java program for pyramid star pattern.
+  
+import java.io.*;
+public class PyramidPattern
+{
+public class void main(String args[])
+{
+  int i,j,row=7;
+  for(i=0;i<row;i++)
+   {
+     System.out.print(" ");
+  }
+  {
+    System.out.print("*");
+  }
+  System.out.println();
+}
+}
+}
+  
